f25cde005a779db48650ded6bbbe1d68f56ef16c
dj.kim <dj.kim@toss.im>
2022-01-10 03:21:42 +0900

    wip

---
+++ app/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+// const
+const PORT = process.env.PORT || 3000
+const SERVICE_NAME = process.env.SERVICE_NAME || 'nodeapp'
+const VERSION = process.env.VERSION || '0.0.1'
+
+app.get('/', (req, res, next) => {
+  const sourceIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(`source ip :: ${sourceIp}`)
+  res.json(`source ip :: ${sourceIp}`)
+})
+
+app.listen(PORT, () => console.log(`server is running on ${PORT}`))
